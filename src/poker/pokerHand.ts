cd1695ec7b96d69c63a8fba6598cf0dc85f260e2
Skymirrh <skymirrh@gmail.com>
2019-04-14 17:55:41 +0200

    Add PokerHand (subclass of Hand with poker ranking data)

---
+++ src/poker/pokerHand.ts
@@ -0,0 +1,18 @@
+import { Card } from './card';
+import CardSet from './cardSet';
+import Hand from './hand';
+import { PokerHandType, PokerHandRanker } from './pokerHandRanker';
+import PokerHandComparer from './pokerHandComparer';
+
+/**
+ * A ranked poker hand, with tiebreaker card data.
+ */
+export default class PokerHand extends Hand {
+  public type: PokerHandType;
+  public tiebreaker: Card[];
+
+  constructor(cardSet: CardSet) {
+    super(cardSet);
+    [this.type, this.tiebreaker] = PokerHandRanker.rank(this);
+  }
+}
