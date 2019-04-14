de0d0f0632cba3c7309b34c8490a44d8ac03b775
Skymirrh <skymirrh@gmail.com>
2019-04-14 17:53:30 +0200

    Add Card class

---
+++ src/poker/card.ts
@@ -0,0 +1,39 @@
+export enum CardValue {
+  Two = 2,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+  Ace,
+}
+
+export enum CardSuit {
+  Spades,
+  Hearts,
+  Diamonds,
+  Clubs,
+}
+
+/**
+ * A card with its value and suit.
+ */
+export class Card {
+  constructor(public value: CardValue, public suit: CardSuit) {}
+  
+  equals(other: Card): boolean {
+    return this.value === other.value && this.suit === other.suit;
+  }
+
+  toString(): string {
+    let value = this.value >= 10 ? CardValue[this.value].toString().substr(0,1) : this.value;
+    let suit = CardSuit[this.suit].toString().substr(0,1);
+    return `${value}${suit}`;
+  }
+}
